d283f31f0ea33ce7da7d2fa6731a26466e0f92ca
Zeffut <tom77ds@gmail.com>
2024-01-17 11:57:07 +0100

    Explosion

--- save_score/save_score.ino
+++ save_score/save_score.ino
@@ -1,21 +1,24 @@
 #include <EEPROM.h>
 
-char pseudo[5][2][20] = {{{"Zeffut0", "0"},
-                          {"Zeffut1", "1"},
-                          {"Zeffut2", "2"},
-                          {"Zeffut3", "3"},
-                          {"Zeffut4", "4"}}}
+char pseudo[5][2] = {{'Test0', '00000'},
+                     {'Test1', '00001'},
+                     {'Test2', '00002'},
+                     {'Test3', '00003'},
+                     {'Test4', '00004'}};
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   for(char i=0; i<5; i++){
     for(char j=0; i<2; j++){
-      for(char k=0; k<1; k++){
-        EEPROM.write[i][j][k]
-      }}}
+      //char valeur[5]={pseudo[i][j][0],pseudo[i][j][1],pseudo[i][j][2],pseudo[i][j][3],pseudo[i][j][4]};
+      Serial.println(pseudo[i][j]);
+      delay(1000);
+      //for (char k=0;k<5;k++){
+      //  EEPROM.write((i+j)*5,valeur[k]);
+      //}
     }
-  }
+  } 
+}
 
 void loop() {
-  Serial.print();
 }
